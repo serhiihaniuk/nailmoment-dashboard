ec649a9c37aa9d4e69c424ea232a8ad070ef5b5f
serhiihaniuk <serhii.haniuk@gmail.com>
2025-06-20 14:26:06 +0200

    add speaker names

--- app/api/tg/speaker-bot/route.ts
+++ app/api/tg/speaker-bot/route.ts
@@ -26,61 +26,61 @@ const WELCOME_MESSAGE_PART_2 = `📹 Відеопрезентації учасн
 const SPEAKERS = [
   {
     id: "video_1",
-    name: "Відео 1",
+    name: "Anna Soloviova",
     file_id:
       "BAACAgIAAxkBAAIBG2hVT71yjORJP9C_6G179cyIh5VuAAIBeQACuEmpSgFPJSaD8w2JNgQ",
   },
   {
     id: "video_2",
-    name: "Відео 2",
+    name: "Таті",
     file_id:
       "BAACAgIAAxkBAAIBHWhVUIa-YTVvO3fu8NeqCeve30rPAAJqbwACUgioSqdJcE2mrBceNgQ",
   },
   {
     id: "video_3",
-    name: "Відео 3",
+    name: "Ася",
     file_id:
       "BAACAgIAAxkBAAIBH2hVUIpvPhjvKYDeGCk8xRnx1JMcAAJrbwACUgioSrgQm0GifuhgNgQ",
   },
   {
     id: "video_4",
-    name: "Відео 4",
+    name: "Ирина Бондарчук",
     file_id:
       "BAACAgIAAxkBAAIBIWhVUI4p3ww4znpnAeW1H-mkLut9AAJtbwACUgioSrYaQI-MuyrANgQ",
   },
   {
     id: "video_5",
-    name: "Відео 5",
+    name: "Настенька Вахулка",
     file_id:
       "BAACAgIAAxkBAAIBI2hVUJLpxgWykIYmLg81yHpQbsBmAAJubwACUgioSoNWibi6PI14NgQ",
   },
   {
     id: "video_6",
-    name: "Відео 6",
+    name: "Sofiko",
     file_id:
       "BAACAgIAAxkBAAIBJWhVUJbeTFeqIqVrv2iou5-xd6SXAAJwbwACUgioSog4y_8mDOWjNgQ",
   },
   {
     id: "video_7",
-    name: "Відео 7",
+    name: "Nataliia Roshchuk",
     file_id:
       "BAACAgIAAxkBAAIBJ2hVUJvoqCdxxGn6D3NP8N0IfscZAAJxbwACUgioSoHJsbCWYHhjNgQ",
   },
   {
     id: "video_8",
-    name: "Відео 8",
+    name: "Katerina",
     file_id:
       "BAACAgIAAxkBAAIBLWhVUKlsaztpb6g2VLCVeu3G3WW1AAJ1bwACUgioSrx1CweRBSrPNgQ",
   },
   {
     id: "video_9",
-    name: "Відео 9",
+    name: "Walentyna",
     file_id:
       "BAACAgIAAxkBAAIBKWhVUJz0L_eLF2m5L8al7u81BPU2AAJybwACUgioSl-oLls7kt0_NgQ",
   },
   {
     id: "video_10",
-    name: "Відео 10",
+    name: "Viктория",
     file_id:
       "BAACAgIAAxkBAAIBK2hVUKKGSc3bouhp9K0qqK7V-HObAAJzbwACUgioSmyeUIFsDly6NgQ",
   },
