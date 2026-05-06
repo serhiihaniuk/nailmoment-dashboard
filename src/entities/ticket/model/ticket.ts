7b77da4a2972994efe3012e04fea5471de386f59
serhiihaniuk <serhii.haniuk@gmail.com>
2026-05-06 17:46:56 +0200

    fix: update standard ticket price

--- src/entities/ticket/model/ticket.ts
+++ src/entities/ticket/model/ticket.ts
@@ -22,7 +22,7 @@ export { TICKET_TYPE, TICKET_TYPE_LIST };
 export type { TicketGrade };
 
 export const TICKET_PRICE_BY_GRADE: Record<TicketGrade, string> = {
-  standard: "399.00",
+  standard: "199.00",
   maxi: "590.00",
   vip: "949.00",
 };
